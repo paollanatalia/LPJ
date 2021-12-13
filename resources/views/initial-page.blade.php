62d6fe0bba139a32ffbbf190ad54beef5e0060ea
paollanatalia <92040265+paollanatalia@users.noreply.github.com>
2021-12-13 11:23:23 -0300

    atualizações

--- resources/views/initial-page.blade.php
+++ resources/views/initial-page.blade.php
@@ -35,7 +35,7 @@
             <div class="container px-4 px-lg-5 text-center">
                 <h1 class="mb-1">LPJ</h1>
                 <h3 class="mb-5"><em>A Free Bootstrap Theme by Start Bootstrap</em></h3>
-                <a class="btn btn-primary btn-xl" href="#about">Find Out More</a>
+                <a class="btn btn-primary btn-xl" href="/login">Login</a>
             </div>
         </header>
         <!-- About-->
