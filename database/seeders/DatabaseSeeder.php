5d3d1c9255536304b440a7501062b89921ea5354
paollanatalia <paola.natalia@escolar.ifrn.edu.br>
2022-01-21 22:29:12 -0300

    seed admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,19 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(LaratrustSeeder::class);
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'medicinecontrolsuporte@gmail.com',
+            'password' => Hash::make('12345678'),
+            'description' => 'Administrador do sistema',
+        ]);
+
+        $user = User::all()->where('name', 'admin')->first();
+
+        DB::table('role_user')->insert([
+            'role_id' => 1,
+            'user_id' => strval($user->id),
+            'user_type' => 'App\Models\User',
+        ]);
     }
 }
